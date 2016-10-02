bb1f045484cd25d14a87ca02a0d6e29a8a8c8888
JacobBrunsting <jacob.brunsting@gmail.com>
2016-10-02 18:01:13 -0400

    Disabled transcript sorting animations on mobile

--- table_animation_setup.js
+++ table_animation_setup.js
@@ -1,7 +1,6 @@
 const ROW_ANIMATION_DURATION = 250;
 const ANIMATION_ACCELERATION_EXPONENT = 1 / 2;
 const ANIMATION_EQUATION_DENOMINATOR = 2 * Math.pow(0.5, ANIMATION_ACCELERATION_EXPONENT);
-
 var currentlyAnimating = false;
 
 function invertHeaderSorting(header, animate) {
@@ -9,7 +8,6 @@ function invertHeaderSorting(header, animate) {
         return;
     }
     currentlyAnimating = true;
-    
     var table = document.getElementById("transcript").children[0];
     var rows = [];
     var rowHeights = [];
@@ -18,34 +16,28 @@ function invertHeaderSorting(header, animate) {
         rows[i] = table.children[i + 1];
         rowHeights[i] = rows[i].offsetHeight;
     }
-    
     var firstTableRow = table.firstChild;
     var tableHeaders = firstTableRow.children;
     var selectedHeaderCell = tableHeaders[header].children[0];
     var selectedHeaderCellArrows = selectedHeaderCell.children[1];
-    
-
     var cellsAtRowHeaderIntersect = [];
     for (var i = 0; i < rows.length; ++i) {
-        cellsAtRowHeaderIntersect.push(rows[i].children[header].children[0].textContent); 
+        cellsAtRowHeaderIntersect.push(rows[i].children[header].children[0].textContent);
     }
-    
     var oldArray = cellsAtRowHeaderIntersect.slice(0);
-    
     if (selectedHeaderCellArrows.className == "arrows sorted_up") {
         cellsAtRowHeaderIntersect.sort(reverseCellComparator);
         selectedHeaderCellArrows.className = "arrows sorted_down";
-    } else {
+    }
+    else {
         cellsAtRowHeaderIntersect.sort(cellComparator);
         selectedHeaderCellArrows.className = "arrows sorted_up";
     }
-    
     for (var i = 0; i < tableHeaders.length; i++) {
         if (i != header) {
             tableHeaders[i].children[0].children[1].className = "arrows neutral";
         }
     }
-    
     var originalPositions = [];
     var rowShifts = [];
     for (var i = 0; i < oldArray.length; ++i) {
@@ -58,41 +50,37 @@ function invertHeaderSorting(header, animate) {
             }
         }
     }
-    
     while (table.firstChild) {
         table.removeChild(table.firstChild);
     }
-
     table.appendChild(firstTableRow);
-    
     for (var i = 0; i < rows.length; ++i) {
         const row = rows[originalPositions[i]];
         table.appendChild(row);
         var posChange = i - originalPositions[i];
         var rowShiftAmount = sum(rowHeights, i, originalPositions[i]);
-        
         if (rowShiftAmount > 0) {
             rowShiftAmount -= rowHeights[i];
-        } else {
+        }
+        else {
             rowShiftAmount += rowHeights[i];
         }
-                
         if (animate) {
             for (var n = 0; n < 3; n++) {
                 var callback = null;
                 if (n == 0) {
-                    callback = function() {
+                    callback = function () {
                         currentlyAnimating = false;
                     }
                 }
                 animatePosChange(rowShiftAmount, row.children[n].children[0], callback);
             }
-        } else {
+        }
+        else {
             currentlyAnimating = false;
         }
     }
 }
-
 // returns the negative if firstindex is larger than last index
 function sum(list, firstIndex, lastIndex) {
     var negate = false;
@@ -102,16 +90,13 @@ function sum(list, firstIndex, lastIndex) {
         lastIndex = temp;
         negate = true;
     }
-    
     var total = 0;
     for (var i = firstIndex; i <= lastIndex; ++i) {
         total += list[i];
     }
-    
     if (negate) {
         total *= -1;
     }
-    
     return total;
 }
 
@@ -121,7 +106,7 @@ function animatePosChange(yChange, element, onComplete) {
     var framesComplete = 0;
     var currentTop = Math.round(yChange).toString() + "px";
     element.style.top = currentTop;
-    
+
     function animate() {
         var offset;
         if (framesComplete >= frameCount) {
@@ -130,16 +115,16 @@ function animatePosChange(yChange, element, onComplete) {
                 onComplete();
             }
             offset = 0;
-        } else {
+        }
+        else {
             var percentDone = framesComplete / frameCount;
             var changePercentage;
-            
             if (percentDone >= 0.5) {
                 changePercentage = Math.pow(percentDone - 0.5, ANIMATION_ACCELERATION_EXPONENT) / ANIMATION_EQUATION_DENOMINATOR + 0.5;
-            } else {
+            }
+            else {
                 changePercentage = -Math.pow(0.5 - percentDone, ANIMATION_ACCELERATION_EXPONENT) / ANIMATION_EQUATION_DENOMINATOR + 0.5;
             }
-            
             offset = yChange * (1 - changePercentage);
             framesComplete++;
         }
@@ -162,9 +147,11 @@ function cellComparator(a, b) {
     const intVal2 = parseInt(b);
     if (!isNaN(intVal1) && isNaN(intVal2)) {
         return -1;
-    } else if (isNaN(intVal1) && !isNaN(intVal2)) {
+    }
+    else if (isNaN(intVal1) && !isNaN(intVal2)) {
         return 1;
-    } else if (!isNaN(intVal1)) {
+    }
+    else if (!isNaN(intVal1)) {
         return b - a;
     }
     return a.localeCompare(b);
@@ -175,14 +162,17 @@ function reverseCellComparator(a, b) {
 }
 
 function setupTableSorting() {
+    var isMobile = false;
+    if (window.matchMedia("all and (max-width: 800px) , all and (max-device-width: 800px)").matches) {
+        isMobile = true;
+    }
     var headers = document.getElementsByTagName("th");
-    
     for (var i = 0; i < headers.length; ++i) {
         const headerNum = i;
-        headers[i].addEventListener("click", function() {invertHeaderSorting(headerNum, true)})
+        headers[i].addEventListener("click", function () {
+            invertHeaderSorting(headerNum, !isMobile)
+        })
     }
 }
-
 setupTableSorting();
-
 invertHeaderSorting(0, false);
